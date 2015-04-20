d356cd9e9120f1cac3d11e1a9f6d90de287ed709
Lynsay Shepherd <Lynsay@users.noreply.github.com>
2015-04-20 10:56:38 +0100

    Create helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,15 @@
+/*
+	File: helloWorld.java
+	Version: 1.0
+	Date: April 2015
+	Author: Lynsay A. Shepherd
+	License: MIT License (MIT)
+	
+	Description:  Hello World test program.  In Java.
+*/
+
+public class helloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+    }
+}
